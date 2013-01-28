a333b17613f154cb79ea6402c86dad49b85c6a08
Rasmus Sten <rasmus@dll.nu>
2013-01-28 12:47:33 +0200

    Initial Commit

---
+++ simpletrasher/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  simpletrasher
+//
+//  Created by Rasmus Sten on 2013-01-28.
+//  Copyright (c) 2013 Rasmus Sten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
